4ab91c7d3474ed3307dbd56518e35b400c915666
barryjoya <bariallai@aol.com>
2022-01-03 22:24:23 -0500

    my third

---
+++ src/SEcondClass.java
@@ -0,0 +1,20 @@
+
+
+public class SEcondClass {
+
+	public static void main(String[] args) {
+		
+		
+		
+
+	}
+
+//	public static void takeScreen() throws IOException {
+//		TakesScreenshot shot = (TakesScreenshot) driver;
+//		
+//	
+//		File file = shot.getScreenshotAs(OutputType.FILE);
+//		
+//		FileUtils.copyFile(file, new File(".\\screena.png"));
+//	}
+}
