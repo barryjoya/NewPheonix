2a227e51d61e17e00d331e8cd04cff46b62f302f
barryjoya <79568230+barryjoya@users.noreply.github.com>
2022-01-04 19:53:54 -0500

    Update hey.java
    added

--- src/hey.java
+++ src/hey.java
@@ -1,15 +1,29 @@
 
 public class hey {
 
+	String name;
+	String lName;
+	String phoneNumber;
+	
   //i brought some changes 
   public hey(){
+	  name ="";
+	  lName ="";
+	  phoneNumber ="";
+	  
   }
    
- 
+ public hey (String name, String lName, String phoneNumber){
+	 this();
+ }
    
 public static void A (){
    System.out.print("Person [name=Jack7, age=28, phone=21654635]"+
 "Person [name=Jack7, age=28, phone=21654635]"+"");
+	
+	
+	
+	
 }
 	   
    }
